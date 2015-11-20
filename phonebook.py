da1d0ec57db181e026318c6f17097093bc70f0f2
Utsav Patel <utsav.me@live.com>
2015-11-21 00:44:15 +0530

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -1,4 +1,6 @@
 import pickle
+import sys
+d1 = {}
 def add_contact():
     while True:
         name = input ("Enter name: ")
@@ -8,36 +10,21 @@ def add_contact():
         if x == "no":
             break
 
-d1={}
-print ("Add contacts first!")
-add_contact()
-
 def call_contact():
     for name, number in d1.items():
         print("The number of {} contact is {}\n".format(name,number))
 
-y = input ('Do you want to see the contacts that you have added? ')
-if y == 'yes':
-    call_contact()
-else:
-    print ("ok")
-
 def del_contacts():
-    for name, number in d1.copy().items():
-        if (del_name == name):
-            del d1[name]
-            print ("Contact deleted!")
-    else:
-        print ("Contact does not exist")
+    while True:
+        del_name = input ("Type the name of the contact you wish to delete")
+        for name, number in d1.copy().items():
+            if (del_name == name):
+                del d1[name]
+                print ("Contact deleted!")
+        z =  input("Do you want to delete some more?")
+        if z == "no":
+            break
 
-z = input ("Do you wish to delete any of the number you added? ")
-
-if z == 'yes':
-    del_name = input ("Type the name of the contact you wish to delete")
-    del_contacts()
-else:
-    print ("ok")
-    
 #--------------storing in file------------------
 contacts = d1
 f = open('contacts.txt','wb')
@@ -47,3 +34,14 @@ f = open('contacts.txt','rb')
 storedlist = pickle.load(f)
 
 print (storedlist)
+
+while True:
+    choice = input("Enter the choice\n 1. Add a contact\n 2. Delete a contact\n 3. View all contacts\n 4. Quit ")
+    if (choice == "1"):
+        add_contact()
+    elif (choice == "2"):
+        del_contacts()
+    elif (choice == "3"):
+        call_contact()
+    else:
+        sys.exit()
